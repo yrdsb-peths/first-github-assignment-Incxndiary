4bc4bb1ec5ff8ac286f335747a5ef320ae485ea6
Incxndiary <150150233+Incxndiary@users.noreply.github.com>
2023-11-08 08:36:42 -0500

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student(){
+  private String firstname;
+  private String lastname;
+  
+  public Student(String f, String l){
+    firstname = f;
+    lastname = l;
+  }
+  public String toString(){
+    return firstname + ", " + lastname;
+  }
+}
