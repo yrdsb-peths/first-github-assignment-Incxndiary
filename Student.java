742186d9a9057462fdd70564fd20051f888100bf
Incxndiary <349070870@gapps.yrdsb.ca>
2023-11-09 22:32:16 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,12 +1,14 @@
 public class Student(){
   private String firstname;
   private String lastname;
+  private int volunteerHours;
   
-  public Student(String f, String l){
+  public Student(String f, String l, int vH){
     firstname = f;
     lastname = l;
+    volunteerHours = vH;
   }
   public String toString(){
-    return firstname + ", " + lastname "goes to P.E.T.H.S.";
+    return firstname + ", " + lastname "goes to P.E.T.H.S. has " + volunteerHours " volunteer hours";
   }
 }
