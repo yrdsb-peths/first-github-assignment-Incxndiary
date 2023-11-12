ef88617a3fcd563d2d5adab512ac3d77d54f8cbf
Incxndiary <150150233+Incxndiary@users.noreply.github.com>
2023-11-11 21:41:09 -0500

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,11 @@
+public class Dog{
+  private String name;
+  private int age;
+  public Dog(String n; int a){
+    name = n;
+    age = a;
+  }
+  public String toString(){
+    return name + ", " + age;
+  }
+}
